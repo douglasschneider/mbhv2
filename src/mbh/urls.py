8be91c3df5bb4fb3509b6bc989c4b435b7fed399
Douglas Schneider <schneider0095@gmail.com>
2020-05-25 19:50:26 -0300

    Alterado a função principal e localização

--- src/mbh/urls.py
+++ src/mbh/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from museu import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
