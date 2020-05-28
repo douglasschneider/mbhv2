d53c2ef5416939ba084bc373f8d3c835c48ad8ef
Douglas Schneider <schneider0095@gmail.com>
2020-05-28 12:43:32 -0300

    Adicionado tabelas de arquivos e visitas

--- Banco de Dados/mbh_tabelas.sql
+++ Banco de Dados/mbh_tabelas.sql
@@ -5,7 +5,7 @@ CREATE TABLE Pessoa(
 	nompes VARCHAR(80) NOT NULL,
 	cpfpes VARCHAR(14) NOT NULL,
 	dtanascpes DATE,
-	sexpes CHAR(1) CONSTRAINT pes_sexpes_ck CHECK(sexpes IN ('F', 'M'),
+	sexpes CHAR(1) CONSTRAINT pes_sexpes_ck CHECK(sexpes IN ('F', 'M')),
 	CONSTRAINT pessoa_pk PRIMARY KEY (codpes)
 );
 
@@ -20,28 +20,32 @@ COMMENT ON COLUMN Email.endema IS 'Endereço do e-mail';
 
 CREATE TABLE Telefone(
 	codtel SERIAL NOT NULL,
-	aretel INTEGER NOT NULL,
-	numtel INTEGER NOT NULL,
+	aretel INTEGER NOT NULL CONSTRAINT tel_aretel_ck CHECK(aretel > 0),
+	numtel INTEGER NOT NULL CONSTRAINT tel_numtel_ck CHECK(numtel > 0),
 	codpes INTEGER NOT NULL,
 	CONSTRAINT telefone_pk PRIMARY KEY (codtel),
 	CONSTRAINT tel_codpes_fk FOREIGN KEY (codpes) REFERENCES Pessoa (codpes) 
 );
 COMMENT ON COLUMN Telefone.aretel IS 'Código de área';
 
-CREATE TABLE Endereco(
-	codend SERIAL NOT NULL,
-	logend VARCHAR(100) NOT NULL,
-	numend INTEGER,
-	compend VARCHAR(30),
-	CONSTRAINT endereco_pk PRIMARY KEY (codend),
-);
-COMMENT ON COLUMN Endereco.logend IS 'Nome do logradouro (Rua)';
-COMMENT ON COLUMN Endereco.compend IS 'Complemente do edereço';
-
 CREATE TABLE Bairro(
 	codbar SERIAL NOT NULL,
 	nombar VARCHAR(70) NOT NULL,
 	CONSTRAINT bairro_pk PRIMARY KEY (codbar)
+);
+
+CREATE TABLE Pais(
+	codpas SERIAL NOT NULL,
+	nompas VARCHAR(50) NOT NULL,
+	CONSTRAINT pais_pk PRIMARY KEY (codpas)
+);
+
+CREATE TABLE Estado(
+	codest SERIAL NOT NULL,
+	ufest VARCHAR(2) NOT NULL,
+	codpas INTEGER NOT NULL,
+	CONSTRAINT estado_pk PRIMARY KEY (codest),
+	CONSTRAINT est_codpas_fk FOREIGN KEY (codest) REFERENCES Pais (codpas)
 );
 
 CREATE TABLE Cidade(
@@ -52,19 +56,21 @@ CREATE TABLE Cidade(
 	CONSTRAINT cid_codest_fk FOREIGN KEY (codest) REFERENCES Estado (codest)
 );
 
-CREATE TABLE Estado(
-	codest SERIAL NOT NULL,
-	nomest VARCHAR(50) NOT NULL,
-	codpas INTEGER NOT NULL,
-	CONSTRAINT estado_pk PRIMARY KEY (codest),
-	CONSTRAINT est_codpas_fk FOREIGN KEY (codest) REFERENCES Pais (codpas)
+CREATE TABLE Endereco(
+	codend SERIAL NOT NULL,
+	logend VARCHAR(100) NOT NULL,
+	numend INTEGER CONSTRAINT end_numend_ck CHECK(numend > 0),
+	compend VARCHAR(30),
+	codbar INTEGER NOT NULL,
+	codcid INTEGER NOT NULL,
+	codpes INTEGER NOT NULL,
+	CONSTRAINT endereco_pk PRIMARY KEY (codend),
+	CONSTRAINT end_codbar_fk FOREIGN KEY (codbar) REFERENCES Bairro (codbar),
+	CONSTRAINT end_codcid_fk FOREIGN KEY (codcid) REFERENCES Cidade (codcid),
+	CONSTRAINT end_codpes_fk FOREIGN KEY (codpes) REFERENCES Pessoa (codpes)
 );
-
-CREATE TABLE Pais(
-	codpas SERIAL NOT NULL,
-	nompas VARCHAR(50) NOT NULL,
-	CONSTRAINT pais_pk PRIMARY KEY (codpas)
-);
+COMMENT ON COLUMN Endereco.logend IS 'Nome do logradouro (Rua)';
+COMMENT ON COLUMN Endereco.compend IS 'Complemento do edereço';
 
 CREATE TABLE Usuario(
 	codusu SERIAL NOT NULL,
@@ -82,6 +88,18 @@ CREATE TABLE Visitante(
 	CONSTRAINT vis_codusu_fk FOREIGN KEY (codusu) REFERENCES Usuario (codusu)
 );
 
+CREATE TABLE Visita(
+	codvst SERIAL NOT NULL,
+	dtavst DATE NOT NULL,
+	hravst TIME NOT NULL,
+	codvis INTEGER NOT NULL,
+	CONSTRAINT visita_pk PRIMARY KEY (codvst),
+	CONSTRAINT vst_codvis_fk FOREIGN KEY (codvis) REFERENCES Visitante (codvis)
+);
+COMMENT TABLE Visita IS 'Agendamento da visita';
+COMMENT COLUMN Visita.dta IS 'Data da visita';
+COMMENT COLUMN Visita.hravst IS 'Horário da visita';
+
 CREATE TABLE Instituicao(
 	codins SERIAL NOT NULL,
 	nomins VARCHAR(80) NOT NULL,
@@ -95,15 +113,15 @@ CREATE TABLE ProprietarioInstituicao(
 	codproins SERIAL NOT NULL,
 	nomproins VARCHAR(30) NOT NULL,
 	codins INTEGER NOT NULL,
-	CONSTRAINT proprietarioinstituicao_fk PRIMARY KEY (codproins),
+	CONSTRAINT proprietarioinstituicao_pk PRIMARY KEY (codproins),
 	CONSTRAINT proins_codins_fk FOREIGN KEY (codins) REFERENCES Instituicao (codins)
 );
-COMMENT ON COLUMN ProprietarioInstitituicao IS 'Nome do proprietário da instituição';
+COMMENT ON COLUMN ProprietarioInstitituicao.nomproins IS 'Nome do proprietário da instituição';
 
 CREATE TABLE Funcionario(
 	codfun SERIAL NOT NULL,
 	fncfun VARCHAR(50) NOT NULL,
-	slafun NUMERIC(19,2) NOT NULL,
+	slafun NUMERIC(19,2) CONSTRAINT fun_slafun_ck CHECK(slafun > 0.00),
 	htbfun TIMESTAMP NOT NULL,
 	dtaadmfun DATE NOT NULL,
 	codpes INTEGER NOT NULL,
@@ -155,6 +173,17 @@ CREATE TABLE Autor(
 
 CREATE TABLE Objeto_Autor(
 	codobj SERIAL NOT NULL,
-	codaut SERIAL,
+	codaut SERIAL NOT NULL,
 	CONSTRAINT objeto_autor_pk PRIMARY KEY (codobj, codaut)
 );
+
+CREATE TABLE Arquivo(
+	codarq SERIAL NOT NULL,
+	nomarq VARCHAR(20) NOT NULL,
+	dtaarq TIMESTAMP NOT NULL,
+	arqarq BYTEA NOT NULL,
+	codobj INTEGER NOT NULL,
+	CONSTRAINT arquivo_pk PRIMARY KEY (codarq),
+	CONSTRAINT arq_codobj_fk FOREIGN KEY (codobj) REFERENCES Objeto (codobj)
+);
+COMMENT ON COLUMN Arquivo.arqarq IS 'Arquivo do objeto';
