765059077e7ed3331b7e80264ee11279124a749f
Douglas Schneider <schneider0095@gmail.com>
2020-05-31 14:15:52 -0300

    Banco de dados conectado

--- src/mbh/settings.py
+++ src/mbh/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'museu.apps.MuseuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'museu.apps.MuseuConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mbh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mbh',
+        'USER': 'douglas',
+        'PASSWORD': 'douglas',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
