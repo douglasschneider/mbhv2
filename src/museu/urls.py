b4d2f6424eb7cc39c7f83816caa806157a536738
Douglas Schneider <schneider0095@gmail.com>
2020-06-05 01:40:22 -0300

    Alterado as rotas das urls

--- src/museu/urls.py
+++ src/museu/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contato', views.contato, name='contato'),
-    path('about', views.sobre, name='about'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('contato/', views.ContatoView.as_view(), name='contato'),
+    path('about/', views.SobreView.as_view(), name='about'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('categories/', views.CategoriaListView.as_view(), name='categories'),
 ]
