a9e29a9e4633332b8dfa031eff2aefb26a3c3271
Douglas Schneider <schneider0095@gmail.com>
2020-06-05 01:39:02 -0300

    Adicionado os links de redirecionamento

--- src/mbh/settings.py
+++ src/mbh/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'categories'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
