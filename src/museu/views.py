99fe39ca5a98a5aee42b71554d8184341065236b
Douglas Schneider <schneider0095@gmail.com>
2020-06-05 01:44:11 -0300

    Criado novas views para exibir as páginas do site

--- src/museu/views.py
+++ src/museu/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse, reverse_lazy
 
+from .models import Categoria
 
 # Create your views here.
-def index(request):
-    return render(request, 'museu/index.html', {})
+class IndexView(generic.TemplateView):
+    template_name = 'museu/index.html'
 
 
-def contato(request):
-    return render(request, 'museu/contato.html', {})
+class ContatoView(generic.TemplateView):
+    template_name = 'museu/contato.html'
 
 
-def sobre(request):
-    return render(request, 'museu/about.html', {})
+class SobreView(generic.TemplateView):
+    template_name = 'museu/about.html'
+
+
+class CategoriaListView(LoginRequiredMixin, generic.ListView):
+    login_url = reverse_lazy('login')
+    model = Categoria
+    template_name = 'museu/categorias.html'
+
