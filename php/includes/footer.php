24f87b17596b9f94bb0f8998da68aac0a4056b34
Douglas Schneider <schneider0095@gmail.com>
2020-06-10 14:31:12 -0300

    Fixado o rodapé ao final da página

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -3,14 +3,15 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="https://getbootstrap.com/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
 
-<footer class="text-muted">
-<br><br>
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Voltar ao topo</a>
-        </p>
-        <p>Copyright - Ciência da Computação UNOESC &copy; Todos os direitos reservados.</p>
-      </div>
-    </footer>
-<br>
+<div class="navbar fixed-bottom">
+  <footer class="text-muted">
+    <br><br>
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Voltar ao topo</a>
+      </p>
+    </div>
+    <p>Copyright - Ciência da Computação UNOESC &copy; Todos os direitos reservados.</p>
+  </footer>
+</div>
 </html>
